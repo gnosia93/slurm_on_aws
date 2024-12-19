9ad2ffaa9f30badd2acf60b77f405b90cd6574a5
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-19 13:08:53 +0900

    Update sla-ec2.tf

--- tf/sla-ec2.tf
+++ tf/sla-ec2.tf
@@ -44,14 +44,6 @@ data "aws_ami" "ubuntu-arm64-nvidia" {
   }
 }
 
-# https://stackoverflow.com/questions/74508452/migrate-from-template-file-to-templatefile
-data "templatefile" "script" {
-  template = "${file("script.tpl")}"
-  vars = {
-    efs_id = "${module.efs.id}"
-  }
-}
-
 
 module "slurm-master" {
   source  = "terraform-aws-modules/ec2-instance/aws"

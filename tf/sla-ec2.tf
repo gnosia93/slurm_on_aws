018c6207453a81d39b77c2a78bdf1af7602ddf18
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-20 12:12:57 +0900

    Update sla-ec2.tf

--- tf/sla-ec2.tf
+++ tf/sla-ec2.tf
@@ -78,7 +78,7 @@ module "slurm-master" {
   provisioner "remote-exec" {  
     inline = [
       "sudo hostnamectl set-hostname sl-${each.key}", 
-      "sudo sed -i '/127.0.0.1 localhost/ s/$/ sl-\${each.key}/' /etc/hosts"
+      "sudo sed -i '/127.0.0.1 localhost/ s/$/ sl-\\${each.key}/' /etc/hosts"
     ]
   }
 
